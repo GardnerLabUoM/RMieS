8c2273aa884d390c37802a68aef96c8eb03a0e9d
Alex Henderson <alex.henderson@manchester.ac.uk>
2017-07-19 18:10:44 +0100

    Added help information and tidied up the layout. No change to functionality.

--- RMieS_EMSC_v5.m
+++ RMieS_EMSC_v5.m
@@ -1,5 +1,66 @@
-function [WN_out ZCorr history] = RMieS_EMSC_v5(WN, ZRaw, correction_options , WN_Ref_in , Ref_in)
-    
+function [WN_out,ZCorr,history] = RMieS_EMSC_v5(WN,ZRaw,correction_options,WN_Ref_in,Ref_in)
+
+% RMieS_EMSC_v5 Resonant Mie Scattering Correction
+% 
+% Syntax
+%   [WN_out,ZCorr,history] =
+%       RMieS_EMSC_v5(WN,ZRaw,correction_options);
+%   [WN_out,ZCorr,history] =
+%       RMieS_EMSC_v5(WN,ZRaw,correction_options,WN_Ref_in,Ref_in);
+%
+% Description
+%   [WN_out,ZCorr,history] = RMieS_EMSC_v5(WN,ZRaw,correction_options)
+%   performs a correction to mid-infrared spectra corresponding to resonant
+%   Mie theory. WN is the wavenumber vector. ZRaw is a 2D matrix containing
+%   absorbance spectra in rows. correction_options is a vector containing
+%   options (see below). WN_out is the wavenumber vector of the corrected
+%   spectra. ZCorr is a 2D matrix of corrected spectra in rows. history is
+%   a 3D matrix of dimension; numberOfSpectra x length(WN_out) x
+%   iterations. 
+%
+%   [WN_out,ZCorr,history] =
+%   RMieS_EMSC_v5(WN,ZRaw,correction_options,WN_Ref_in,Ref_in) utilises a
+%   user supplied reference spectrum. WN_Ref_in is a wavenumber vector
+%   corresponding to the reference spectrum. Ref_in is a vector of absorbance
+%   values of the reference sample.
+%
+%   correction options is a vector with the following default parameters:
+%   correction_options = [ ...
+%     0    ; % 1. Desired resolution, (0 keeps original resolution)
+%     1000 ; % 2. Lower wavenumber range (min value is 1000)
+%     4000 ; % 3. Upper wavenumber range (max value is 4000)
+%     1    ; % 4. Number of iterations
+%     2    ; % 5. Mie theory option (smooth or RMieS)
+%     7    ; % 6. Number of principal components used (8 recommended)
+%     2    ; % 7. Lower range for scattering particle diameter / um
+%     8    ; % 8. Upper range for scattering particle diamter / um
+%     1.1  ; % 9. Lower range for average refractive index
+%     1.5  ; % 10. Upper range for average refractive index
+%     10   ; % 11. Number of values for each scattering parameter default 10
+%     1    ; % 12. Orthogonalisation, 0 = no, 1 = yes. (1 recommended)
+%     1   ]; % 13. Which reference spectrum, 1 = Matrigel, 2 = Simulated
+%
+%   The correction_options must be supplied by the user, defaults are not
+%   provided by this function. 
+%
+% Notes
+%   Further details of the RMieS method can be found in:
+%    Resonant Mie Scattering (RMieS) correction of infrared spectra from
+%    highly scattering biological samples. Paul Bassan, Achim Kohler,
+%    Harald Martens, Joe Lee, Hugh J. Byrne, Paul Dumas, Ehsan Gazi,
+%    Michael Brown, Noel Clarke and Peter Gardner 
+%    Analyst 135 (2010) 268-277. https://doi.org/10.1039/B921056C
+%   
+%   Source code available from https://github.com/GardnerLabUoM/RMieS
+%
+% Copyright (c) 2010 Paul Bassan
+% Modifications Copyright (c) 2017 Alex Henderson
+% Licenced under the GNU Lesser General Public License (LGPL) version 3.
+
+% Contact email: Alex Henderson <alex.henderson@manchester.ac.uk>
+% Licenced under the GNU Lesser General Public License (LGPL) version 3
+% https://www.gnu.org/copyleft/lesser.html
+% Other licensing options are available, please contact Alex for details
 
 
 %% Designate correction options
@@ -22,60 +83,62 @@ ref_option = correction_options(13);
 
 WN = make_column(WN);
 
-if desired_res > 0
-    [WN ZRaw] = new_res(WN , ZRaw , desired_res);
+if (desired_res > 0)
+    [WN,ZRaw] = new_res(WN, ZRaw, desired_res);
 end
 
-
-[a b] = find_value_min_max( WN , lower_WN , upper_WN);
+[a,b] = find_value_min_max(WN, lower_WN, upper_WN);
 
 WN = WN(a:b);
-ZRaw = ZRaw(:,a:b);
+ZRaw = ZRaw(:, a:b);
 
-[N K] = size(ZRaw);
-spectra_names = num2str( [1:N]' );
+[N,K] = size(ZRaw);
+spectra_names = num2str([1:N]');
 
 %% Reference spectrum
 
-if nargin == 3;
-    if ref_option == 1;
+if (nargin == 3)
+    if (ref_option == 1)
         load Matrigel_Reference_Raw
-        ZRef = spline( ZRef_Raw(:,1) , ZRef_Raw(:,2) , WN )'; clear ZRef_Raw
-    elseif ref_option == 2;
+        ZRef = spline(ZRef_Raw(:,1), ZRef_Raw(:,2), WN)';
+        clear ZRef_Raw
+    elseif (ref_option == 2)
         load Simulated_ZRef_Raw; 
-        ZRef = spline( ZRef_Raw(:,1) , ZRef_Raw(:,2) , WN )'; clear ZRef_Raw
+        ZRef = spline(ZRef_Raw(:,1), ZRef_Raw(:,2), WN)'; 
+        clear ZRef_Raw
     else
         error('No reference spectrum option selected')
     end
 end
 
-
-
-if nargin == 5;
-    
-    if length(WN) == length(WN_Ref_in)
+if (nargin == 5)
+    if (length(WN) == length(WN_Ref_in))
         ref_length_check = 1;
     else
         ref_length_check = 0;
     end
     
-    if ref_length_check == 0;        
-        ZRef = spline( WN_Ref_in , Ref_in , WN )';
+    if (ref_length_check == 0)
+        ZRef = spline(WN_Ref_in, Ref_in, WN)';
     end
 end
 
-
-ZRef = ZRef/max(ZRef);
+ZRef = ZRef / max(ZRef);
 
 %% Make Saisir Structures & Weights
 
-temp = ZRaw; clear ZRaw;
+temp = ZRaw; 
+clear ZRaw;
 
-ZRaw.d = temp; clear temp;
+ZRaw.d = temp; 
+clear temp;
+
 ZRaw.v = num2str(WN);
 ZRaw.i = spectra_names;
 
-temp = ZRef; clear ZRef
+temp = ZRef; 
+clear ZRef
+
 ZRef.d = temp;
 ZRef.v = ZRaw.v;
 ZRef.i = 'Ref';
@@ -84,90 +147,52 @@ initial_ref = ZRef;
 
 ZWeightsSpec = Spectrum_Weights(ZRaw);
 
-ZWeightsSpec = Down_weight_spectrum(ZWeightsSpec , 2300 , 2400 , 0.01);
+ZWeightsSpec = Down_weight_spectrum(ZWeightsSpec, 2300, 2400, 0.01);
 
-if mie_theory == 1;
-    ZWeightsSpec = Down_weight_spectrum(ZWeightsSpec , 1500 , 2000 , 0.01);
+if (mie_theory == 1)
+    ZWeightsSpec = Down_weight_spectrum(ZWeightsSpec, 1500, 2000, 0.01);
 end
 
-if iterations > 1;
-    history = zeros(N , K , iterations);
-    
+if (iterations > 1)
+    history = zeros(N, K, iterations);    
 end
 
 %% Correction
 
-if mie_theory == 1
-    [ZCorr] = Mie_maths( ZRef , ZRaw , ZWeightsSpec , ...
-        NCOMP, mie_theory, GSP_flag , r_min , r_max , n_min , n_max , spacings);
+if (mie_theory == 1)
+    ZCorr = Mie_maths(ZRef, ZRaw, ZWeightsSpec, NCOMP, mie_theory, ...
+                        GSP_flag, r_min, r_max, n_min, n_max, spacings);
     history = ZCorr.d;
-elseif mie_theory == 2
-    [ZCorr mod_para] = Mie_maths( ZRef , ZRaw , ZWeightsSpec , ...
-        NCOMP, mie_theory, GSP_flag , r_min , r_max , n_min , n_max , spacings);
+elseif (mie_theory == 2)
+    [ZCorr,mod_para] = Mie_maths(ZRef, ZRaw, ZWeightsSpec, ...
+        NCOMP, mie_theory, GSP_flag, r_min, r_max, n_min, n_max, spacings);
     history(:,:,1) = ZCorr.d;
     res_hist(:,:,1) = mod_para.residual;
     
-    if iterations > 1
-        
-            
-
-        
-        for j = 1 : N
-
-            ZRaw2.d = ZRaw.d(j,:);
-
-            for i = 2 : iterations
+    if (iterations > 1)
+        for j = 1:N
+            ZRaw2.d = ZRaw.d(j, :);
+            for i = 2:iterations
                 try
+                    ZRef.d = history(j, :, i-1);
 
-                    ZRef.d = history(j,:,i-1);
-
-                    %ZRef.d = fit_gauss_whole_spec( WN , ZRef.d')';
-                    ZRef.d = fit_gauss_whole_spec_split( WN , ZRef.d')';
-
-
-                    %ZRef.d = rb_v5( WN , ZRef.d );
-
-
-                    %ZRef.d = build_spectrum_split(WN , ZRef.d , 15 , -0.005);
-                    %ZRef.d = abs(ZRef.d);
-
-
-                    [ZCorr mod_para] = Mie_maths( ZRef , ZRaw2 , ZWeightsSpec , ...
-                        NCOMP, mie_theory, GSP_flag , r_min , r_max , n_min , n_max , spacings);
+                    ZRef.d = fit_gauss_whole_spec_split(WN, ZRef.d')';
+                    [ZCorr,mod_para] = Mie_maths(ZRef, ZRaw2, ZWeightsSpec, ...
+                        NCOMP, mie_theory, GSP_flag, r_min, r_max, n_min, n_max, spacings);
                     history(j,:,i) = ZCorr.d;
 
-
-                    disp(['Spectrum ',num2str(j),' Iteration ',num2str(i),'  ', datestr(now)])
+                    disp(['Spectrum ', num2str(j), ' Iteration ', num2str(i), '  ', datestr(now)])
                 end
             end
-
-            ZCorr.d = history(:,:,end);
+            ZCorr.d = history(:, :, end);
             disp(' ')
         end
-    
     end
-    
 end
 
-
-ZCorr.d = history(:,:,end);
+ZCorr.d = history(:, :, end);
 
 WN_out = WN;
 ZCorr = ZCorr.d;
 
-
-
-
-
-end % end of function
-
-
-
-
-
-
-
-
-
-
-
+end % end of function RMieS_EMSC_v5
